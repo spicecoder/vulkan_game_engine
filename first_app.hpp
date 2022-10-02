1c4467a39e1693be35cb005ebc235128b72a1146
ppal4396 <ppal4396@uni.sydney.edu.au>
2022-10-02 11:53:40 +1100

    initial, generates window

---
+++ first_app.hpp
@@ -0,0 +1,13 @@
+#include "lve_window.hpp"
+
+namespace lve {
+class FirstApp {
+    public:
+        static constexpr int WIDTH = 800;
+        static constexpr int HEIGHT = 600;
+
+        void run();
+    private:
+        LveWindow lveWindow{WIDTH, HEIGHT, "Hello Vulkan!"};
+};
+}
